0e6d644a6a330b95037a722ea5f7a57413d06d24
Dhia Laamari <dhialaamari99991277@gmail.com>
2025-06-16 03:34:33 +0200

    Fix: properly define HTMLDivElement as global

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,32 +1,20 @@
-const fs = require('fs');
-
 module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
-  globals: fs
-    .readFileSync('.eslintglobals', 'utf-8')
-    .split('\n')
-    .filter(Boolean)
-    .reduce((acc, line) => {
-      const [key, value] = line.split(':');
-      acc[key.trim()] = value.trim();
-      return acc;
-    }, {}),
+  globals: {
+    HTMLDivElement: 'readonly',
+  },
   extends: [
-    'next/core-web-vitals',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'next/core-web-vitals'
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: { jsx: true },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
   plugins: ['@typescript-eslint', 'react'],
   rules: {},
 };
