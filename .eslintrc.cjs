2dd3131ea02f71ff18301fdaf81e8faefc14cd1b
Dhia Laamari <dhialaamari99991277@gmail.com>
2025-06-16 03:23:46 +0200

    Fix: recognize HTMLDivElement globally

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,25 +2,25 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
   },
   globals: {
     HTMLDivElement: 'readonly',
   },
   extends: [
+    'next/core-web-vitals',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
   ],
-  plugins: ['@typescript-eslint', 'react'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
     },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
+  plugins: ['@typescript-eslint', 'react'],
   rules: {},
 }
